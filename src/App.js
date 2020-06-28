aab47c06f1bb4a1c3d7c75e5b582fae0c7689f2c
Miguel <miguevalencia29@hotmail.com>
2020-06-28 17:58:36 -0500

    Agregando login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
+import Registrarse from "./pages/Registrarse/Registrarse";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/registrarse" component={Registrarse} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+    return ( <p>Pagina no encontrada</p> );
+}
+ 
+export default NotFound;
